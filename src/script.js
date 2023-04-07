cbec0a78a1ef6978b5e56310c228cb09f469072d
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-08 01:40:21 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@ const a = document.getElementById('testation');
 a.addEventListener("click", () => {
     const documents = {'min': document.getElementById('lenghtmin').value,'max': document.getElementById('lenghtmax').value, 'text': document.getElementById('text')}
     if (documents['min'] != '' && documents['max'] != '') {
-    documents['text'].innerHTML = 'Votre mot de passe est : ' + random(documents['min'], documents['max'])
+    documents['text'].innerHTML = 'Votre nombre est : ' + random(documents['min'], documents['max'])
     documents['text'].setAttribute('class', 'flex')
     } else {
         alert('Veuillez transmettre toutes les informations demandées')
